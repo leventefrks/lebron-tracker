068928a2575915dde1aad79dda4966d98892baf2
leventefrks <levente.f@gmail.com>
2023-02-03 14:48:44 +0100

    count up

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -5,7 +5,7 @@ const Scores = ({ item }) => {
     <li className="py-2 sm:py-5">
       <div className="text-xl uppercase">{item.title}</div>
       <span className="text-4xl font-black tracking-wider">
-        <CountUp start={0} end={item.statistics}>
+        <CountUp start={0} end={item.statistics} delay={0}>
           {({ countUpRef }) => <span ref={countUpRef}>{item.statistics}</span>}
         </CountUp>
       </span>
