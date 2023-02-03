99821d5be4f768125330b06ba9f1ba31cf477ebe
leventefrks <levente.f@gmail.com>
2023-02-03 13:29:31 +0100

    renaming statistics to stats

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -7,9 +7,9 @@ const Scores = ({ item }) => {
     <li className="py-2 sm:py-5">
       <div className="text-xl uppercase">{item.title}</div>
       <span className="text-4xl font-black tracking-wider">
-        <CountUp start={0} end={numberCast(item.statistics)}>
+        <CountUp start={0} end={numberCast(item.stat)}>
           {({ countUpRef }) => (
-            <span ref={countUpRef}>{numberCast(item.statistics)}</span>
+            <span ref={countUpRef}>{numberCast(item.stat)}</span>
           )}
         </CountUp>
       </span>
