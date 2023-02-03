f6abcb69b4a479db1057a8be49c2327a96be2c1c
leventefrks <levente.f@gmail.com>
2023-02-03 23:25:41 +0100

    _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,11 +2,12 @@ import { GoogleAnalytics } from 'nextjs-google-analytics';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
-  return;
-  <>
-    <GoogleAnalytics trackPageViews />
-    <Component {...pageProps} />;
-  </>;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default App;
