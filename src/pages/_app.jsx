fec848e8529f2ba113f9e4b3806c81ad8a32f7d2
leventefrks <levente.f@gmail.com>
2023-02-03 23:20:14 +0100

    add google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,12 @@
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return;
+  <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />;
+  </>;
 };
 
 export default App;
