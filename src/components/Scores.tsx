8040cc307570312d3d0bd05d15bd4ce89780b316
leventefrks <levente.f@gmail.com>
2023-02-03 11:49:16 +0100

    stat

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -8,17 +8,19 @@ interface Item {
 interface ScoresProps {
   item: Item;
 }
-const numberCast = (value: string) => Number(value.replace(',', '')) || '';
 
 const Scores: React.FC<ScoresProps> = ({ item }) => {
+  const numberCast = (value: { stat: string }): number =>
+    Number(value.stat.replace(',', '')) || '';
+
+  const stat: number = numberCast(item);
+
   return (
     <li className="py-2 sm:py-5">
       <div className="text-xl uppercase">{item.title}</div>
       <span className="text-4xl font-black tracking-wider">
-        <CountUp start={0} end={numberCast(item.stat)}>
-          {({ countUpRef }) => (
-            <span ref={countUpRef}>{numberCast(item.stat)}</span>
-          )}
+        <CountUp start={0} end={stat}>
+          {({ countUpRef }) => <span ref={countUpRef}>{stat}</span>}
         </CountUp>
       </span>
     </li>
