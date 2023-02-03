ce6b7026b745b95352f16718ffb25597ea271080
leventefrks <levente.f@gmail.com>
2023-02-03 14:31:37 +0100

    404

---
+++ src/pages/404.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Scores = ({ item }) => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 px-4">
+      <Image
+        className="mx-auto block"
+        src="/lebron-down.webp"
+        alt="LeBron James"
+        width={340}
+        height={340}
+      />
+      <p className="text-xl text-gray-800">Ooops! LeBron is down</p>
+      <Link href="/" className="text-small font-black text-gray-800">
+        Go back
+      </Link>
+    </div>
+  );
+};
+
+export default Scores;
