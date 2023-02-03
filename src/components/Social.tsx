c3c7c82ca610c099ef7dd31b6a732090439cb7be
leventefrks <levente.f@gmail.com>
2023-02-03 09:31:07 +0100

    social link fix

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,19 +3,19 @@ import { AiFillGithub, AiFillTwitterCircle } from 'react-icons/ai';
 const Social = () => {
   return (
     <footer className="absolute top-4 left-4 flex gap-4 text-gray-800">
-      <a href="https://github.com/leventefrks/lebron-tracker">
-        <AiFillGithub
-          className="h-8 w-8 transform duration-200 hover:scale-110"
-          target="_blank"
-          rel="noreferrer"
-        />
+      <a
+        href="https://github.com/leventefrks/lebron-tracker"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <AiFillGithub className="h-8 w-8 transform duration-200 hover:scale-110" />
       </a>
-      <a href="https://twitter.com/twevente_wolf">
-        <AiFillTwitterCircle
-          className="h-8 w-8 transform duration-200 hover:scale-110"
-          target="_blank"
-          rel="noreferrer"
-        />
+      <a
+        href="https://twitter.com/twevente_wolf"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <AiFillTwitterCircle className="h-8 w-8 transform duration-200 hover:scale-110" />
       </a>
     </footer>
   );
